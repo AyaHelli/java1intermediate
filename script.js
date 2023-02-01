a4c7f989b35760bdb8279f885af9ac94fcd5eb5f
AyaHelli <ayahilly_96hotmail.com>
2023-02-01 14:18:03 +0100

    wrong split

---
+++ script.js
@@ -0,0 +1,13 @@
+let cars = ["Tesla", "Audi", " Renault", "Volvo", "Mazda", "Fiat", " Ferrari"];
+console.log(cars);
+let fruits = ["apple", " banana", " kiwi"];
+console.log(fruits.push("orange"));
+console.table(fruits)
+console.log(fruits.pop("orange"));
+console.table(fruits)
+let animals = ["monkey", "horse", "dog"];
+console.log(animals.push("cat"));
+console.log(animals.sort());
+let obst = ["mango/cherries/kiwi/grapes/pear/peach/orange/lemon"];
+let result = obst.split(/).join(" ");
+        console.log(result)
